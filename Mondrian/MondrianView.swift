dc2901b25d5308749e5fbf50d260d80702a3416c
daobao <lucashongyuanzhang@gmail.com>
2020-01-12 09:49:07 +0800

    we added a View

---
+++ Mondrian/MondrianView.swift
@@ -0,0 +1,21 @@
+//
+//  MondrianView.swift
+//  Mondrian
+//
+//  Created by daobao on 2020/1/5.
+//  Copyright © 2020 Grace Huang. All rights reserved.
+//
+
+import UIKit
+
+class MondrianView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
