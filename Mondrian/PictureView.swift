488790cec2930a9f1361c588fbe984221f5da376
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-01-12 20:29:17 -0600

    made random and handled half of rectangle

--- Mondrian/PictureView.swift
+++ Mondrian/PictureView.swift
@@ -10,43 +10,41 @@ import UIKit
 
 class PictureView: UIView {
     override func draw(_ rect: CGRect) {
-        let rectBig:CGRect = CGRect(x: 50, y: 50, width: 650, height: 900)
-        let pen = UIBezierPath(rect: rectBig)
-        pen.stroke()
+//        let rectBig:CGRect = CGRect(x: 50, y: 50, width: 650, height: 900)
+//        let pen = UIBezierPath(rect: rectBig)
+//        pen.stroke()
         
-        handleRect(rect: rectBig)
+        handleRect(rect: rect)
     }
     
+    var count = 0
     func handleRect(rect: CGRect) { // HW: add logic to check vertical or horizontal to add
-        
+        if count > 9 {
+            return
+        }
+        count += 1
         
         let pen = UIBezierPath()
         let rect1: CGRect
         let rect2: CGRect
+        let randomNumber: CGFloat = CGFloat(arc4random() % UInt32(rect.height))
         
         if rect.width < rect.height {
-            pen.move(to: CGPoint(x: rect.origin.x, y: rect.origin.y + rect.height / 3))
-            pen.addLine(to: CGPoint(x: rect.origin.x + rect.width, y: rect.origin.y + rect.height / 3))
-            rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width, height: rect.height / 3)
-            rect2 = CGRect(x: rect.origin.x, y: rect.origin.y + 200, width: rect.width, height: rect.height / 3)
-            
+            pen.move(to: CGPoint(x: rect.origin.x, y: rect.origin.y + randomNumber))
+            pen.addLine(to: CGPoint(x: rect.origin.x + rect.width, y: rect.origin.y + randomNumber))
+            rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width, height: randomNumber)
+            rect2 = CGRect(x: rect.origin.x, y: rect.origin.y + randomNumber, width: rect.width, height: randomNumber)
         } else {
-            pen.move(to: CGPoint(x: rect.origin.x + rect.width / 3, y: rect.origin.y))
-            pen.addLine(to: CGPoint(x: rect.origin.x + rect.width / 3, y: rect.origin.y + rect.height))
-            rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width / 3, height: rect.height)
-            rect2 = CGRect(x: rect.origin.x + rect.width / 3, y: rect.origin.y, width: rect.width  - rect.width / 3, height: rect.height)
+            pen.move(to: CGPoint(x: rect.origin.x + randomNumber, y: rect.origin.y))
+            pen.addLine(to: CGPoint(x: rect.origin.x + randomNumber, y: rect.origin.y + rect.height))
+            rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: randomNumber, height: rect.height)
+            rect2 = CGRect(x: rect.origin.x + randomNumber, y: rect.origin.y, width: rect.width  - randomNumber, height: rect.height)
         }
+        pen.lineWidth = 3
         pen.stroke()
 
-        if rect1.width < 100 || rect1.height < 100{
-            return
-        }
-
-        if rect2.width < 100 || rect2.height < 100{
-            return
-        }
         handleRect(rect: rect1)
-        handleRect(rect: rect2)
+//        handleRect(rect: rect2)
         
     }
 }
