35ec2ea59de97cdaeb10f1149103644c9c3c925b
Zhijun Sheng <zhijunsheng@gmail.com>
2020-01-15 18:53:08 -0500

    added random colors for Piet Mondrian art

--- Mondrian/PietView.swift
+++ Mondrian/PietView.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class PietView: UIView {
+    let lineWidth: CGFloat = 11
+    let percent: CGFloat = 0.46
+    var minW: CGFloat = 0
+    var minH: CGFloat = 0
 
     override func draw(_ rect: CGRect) {
-//        drawLineIn(rect: CGRect(x: 0, y: 0, width: rect.width, height: rect.height / 2))
-        drawLineIn(rect: rect)
+        minW = rect.width * percent
+        minH = rect.height * percent
+        
+        let inset = -lineWidth/2
+        let rectWithInset = rect.inset(by: UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset))
+        drawLineIn(rect: rectWithInset)
     }
 
     func drawLineIn(rect: CGRect) {
-        if rect.width < 350 && rect.height < 350 {
+        if rect.width < minW && rect.height < minH {
             return
         }
         
@@ -45,19 +53,27 @@ class PietView: UIView {
             rect1 = CGRect(x: rect.origin.x + randomOffset, y: rect.origin.y, width: rect.width - randomOffset, height: rect.height)
         }
         
-//        let rect0Path = UIBezierPath(rect: rect0)
-//        UIColor.green.setFill()
-//        rect0Path.fill()
-//        let rect1Path = UIBezierPath(rect: rect1)
-//        UIColor.yellow.setFill()
-//        rect1Path.fill()
+        fillIn(rect: rect0, inset: lineWidth/2)
+        fillIn(rect: rect1, inset: lineWidth/2)
         
         path.move(to: CGPoint(x: fromX, y: fromY))
         path.addLine(to: CGPoint(x: toX, y: toY))
-        path.lineWidth = 7
+        path.lineWidth = lineWidth
         path.stroke()
         
         drawLineIn(rect: rect0)
         drawLineIn(rect: rect1)
+    }
+    
+    private func fillIn(rect: CGRect, inset: CGFloat) {
+        let rectWithInset = rect.inset(by: UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset))
+        let rectPath = UIBezierPath(rect: rectWithInset)
+        let r = CGFloat(Int(arc4random()) % 1000) / 1000
+        let g = CGFloat(Int(arc4random()) % 1000) / 1000
+        let b = CGFloat(Int(arc4random()) % 1000) / 1000
+        let a = CGFloat(Int(arc4random()) % 1000) / 1000
+        let color = UIColor(red: r, green: g, blue: b, alpha: a)
+        color.setFill()
+        rectPath.fill()
     }
 }
