5813c3de9cfb932d970e59b5075dbc997e3e1477
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-01-18 21:48:11 +0800

    We made a fat ractangle and a thin rectangle .

--- Mondrian/MondrianView.swift
+++ Mondrian/MondrianView.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class MondrianView: UIView {
     override func draw(_ rect: CGRect) {
-
-        let pencil = UIBezierPath()
-        
-        pencil.move(to: CGPoint(x:234, y: 2))
-        pencil.addLine(to: CGPoint(x: 234, y: 345))
+        let r = CGRect(x: 97, y: 56, width:  580, height:560)
+        let pencil = UIBezierPath(rect: r)
       
-        
-        
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
-         pencil.stroke()
-        
+        pencil.stroke()
+        drawLine(rectangle: r)
     }
+    
+    func drawLine(rectangle: CGRect)  {
+        let pencil = UIBezierPath()
+        let randomOffset =  CGFloat(arc4random() % UInt32(rectangle.height))
+//        pencil.move(to: CGPoint(x: rectangle.origin.x, y: randomOffset + rectangle.origin.y ))
+//        pencil.addLine(to: CGPoint(x: rectangle.origin.x + rectangle.width, y: randomOffset + rectangle.origin.y))
+//
+        let rect0 = CGRect(x: rectangle.origin.x, y: rectangle.origin.y , width: rectangle.width, height: randomOffset)
+        let rect0Path = UIBezierPath(rect: rect0)
+        #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).setFill()
+//        rect0Path.fill()
+        let rect1 = CGRect(x: rectangle.origin.x, y: rectangle.origin.y + randomOffset , width: rectangle.width, height:rectangle.height - randomOffset)
+        let rect1path = UIBezierPath(rect: rect1)
+        #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1).setFill()
+//        rect1path.fill()
+        
+        if rectangle.width > rectangle.height {
+            pencil.move(to: CGPoint(x: randomOffset + rectangle.origin.x, y: rectangle.origin.y))
+            pencil.addLine(to: CGPoint(x: randomOffset + rectangle.origin.x, y: rectangle.origin.y + rectangle.height))
+        } else {
+            pencil.move(to: CGPoint(x: rectangle.origin.x, y: rectangle.origin.y + randomOffset))
+            pencil.addLine(to: CGPoint(x: rectangle.origin.x + rectangle.width, y: rectangle.origin.y + randomOffset))
+        }
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        pencil.stroke()
+    }
+    
+   
 }
+
