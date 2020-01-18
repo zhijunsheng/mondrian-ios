d21bd3f52e725e159e481af1626ba30ee5a3fefc
daobao <lucashongyuanzhang@gmail.com>
2020-01-18 09:21:21 +0800

    we finished the mondrian picture

--- Mondrian/MondrianView.swift
+++ Mondrian/MondrianView.swift
@@ -8,7 +8,7 @@ class MondrianView: UIView {
     
     override func draw(_ rect: CGRect) {
         
-        drawMondrianLineIn(rect: rect)
+        drawMondrianLineIn(rect: rect.inset(by: UIEdgeInsets(top: -6.125, left: -6.125, bottom: -6.125, right: -6.125)))
     }
     
     func drawMondrianLineIn(rect: CGRect) {
@@ -30,24 +30,24 @@ class MondrianView: UIView {
             let mondrianRandomLine = UIBezierPath()
             mondrianRandomLine.move(to: CGPoint(x: mondrianX1, y: mondrianY1))
             mondrianRandomLine.addLine(to: CGPoint(x: mondrianX2, y: mondrianY2))
-            mondrianRandomLine.lineWidth = 6.125
-            mondrianRandomLine.stroke()
+            mondrianRandomLine.lineWidth = 12.25
+            
             
             let rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: mondrianX1 - rect.origin.x, height: rect.height)
             let rect2 = CGRect(x: mondrianX2, y: rect.origin.y, width: rect.origin.x + rect.width - mondrianX2, height: rect.height)
             
+            
+            
+            let mondriancolor1 = UIBezierPath(rect: rect1.inset(by: UIEdgeInsets(top: 6.125, left: 6.125, bottom: 6.125, right: 6.125)))
+            #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1).setFill()
+            mondriancolor1.fill()
+
+            let mondriancolor2 = UIBezierPath(rect: rect2.inset(by: UIEdgeInsets(top: 6.125, left: 6.125, bottom: 6.125, right: 6.125)))
+            #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1).setFill()
+            mondriancolor2.fill()
+            mondrianRandomLine.stroke()
             drawMondrianLineIn(rect: rect1)
             drawMondrianLineIn(rect: rect2)
-            
-//            let mondriancolor1 = UIBezierPath(rect: rect1)
-//            #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 0.5).setFill()
-//            mondriancolor1.fill()
-//
-//            let mondriancolor2 = UIBezierPath(rect: rect2)
-//            #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 0.5).setFill()
-//            mondriancolor2.fill()
-            
-            
         } else {
             let mondrianX1: CGFloat = rect.origin.x
             let mondrianY1: CGFloat = CGFloat((arc4random() % UInt32(rect.height))) + rect.origin.y
@@ -58,19 +58,20 @@ class MondrianView: UIView {
             let mondrianRandomLine = UIBezierPath()
             mondrianRandomLine.move(to: CGPoint(x: mondrianX1, y: mondrianY1))
             mondrianRandomLine.addLine(to: CGPoint(x: mondrianX2, y: mondrianY2))
-            mondrianRandomLine.lineWidth = 6.125
-            mondrianRandomLine.stroke()
+            mondrianRandomLine.lineWidth = 12.25
+            
             
             let rect1 = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width, height: mondrianY1 - rect.origin.y)
             let rect2 = CGRect(x: rect.origin.x, y: mondrianY1, width: rect.width, height: rect.height - mondrianY1 + rect.origin.y)
             
-//            let mondriancolor1 = UIBezierPath(rect: rect1)
-//            #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 0.5).setFill()
-//            mondriancolor1.fill()
-//
-//            let mondriancolor2 = UIBezierPath(rect: rect2)
-//            #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 0.5).setFill()
-//            mondriancolor2.fill()
+            let mondriancolor1 = UIBezierPath(rect: rect1.inset(by: UIEdgeInsets(top: 6.125, left: 6.125, bottom: 6.125, right: 6.125)))
+            #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).setFill()
+            mondriancolor1.fill()
+
+            let mondriancolor2 = UIBezierPath(rect: rect2.inset(by: UIEdgeInsets(top: 6.125, left: 6.125, bottom: 6.125, right: 6.125)))
+            #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).setFill()
+            mondriancolor2.fill()
+            mondrianRandomLine.stroke()
             drawMondrianLineIn(rect: rect1)
             drawMondrianLineIn(rect: rect2)
         }
